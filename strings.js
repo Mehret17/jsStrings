012a040814a53bb38c0dfc83ec75c40304acff33
Mehret Kulala <merutaye@gmail.com>
2018-02-26 20:43:33 -0600

    compeletd excercise 3

--- strings.js
+++ strings.js
@@ -2,6 +2,13 @@
 var numofSandwiches = (2*4)
 console.log("numofSandwithces", numofSandwiches);
 
+// challenge #2
 var name = "Zoe";
 var string = "Hello, "+ name + "how are you doing today";
 console.log ("string:",string);
+
+// challenge #3
+var DNA = "GCAT";
+var RNA = DNA.replace ('T', 'U');
+// var RNA = DNA.replace (T/g, 'U');
+console.log ("RNA", RNA);
