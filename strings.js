215b0f38bbf9a7e9447afdde70ae0173ecfa60ec
Mehret Kulala <merutaye@gmail.com>
2018-02-26 20:31:36 -0600

    compeleted excercise 2

--- strings.js
+++ strings.js
@@ -1,2 +1,7 @@
+// challenge #1
 var numofSandwiches = (2*4)
 console.log("numofSandwithces", numofSandwiches);
+
+var name = "Zoe";
+var string = "Hello, "+ name + "how are you doing today";
+console.log ("string:",string);
